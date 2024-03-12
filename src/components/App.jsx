b6fb7ec4fdc1e19b448172b799b93dbf2934e1a1
ehoang0106 <khoahoang1698@gmail.com>
2024-03-11 20:31:39 -0700

    complete todo list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+    const [inputText, setInputText] = useState("");
+    const [items, setItems] = useState([]);
+
+
+    function handleChange(event) {
+        const newValue = event.target.value;
+        setInputText(newValue);
+    }
+
+    function addItem() {
+        setItems((prevItems) => {
+            return [...prevItems, inputText];
+        });
+
+        setInputText(""); //when the item added set the inputText to empty string
+    }
+
     return (
         <div className="container">
             <div className="heading">
                 <h1>To-Do List</h1>
             </div>
             <div className="form">
-                <input type="text" />
-                <button>
+                <input type="text" value={inputText} onChange={handleChange}/>
+                <button onClick={addItem}>
                     <span>Add</span>
                 </button>
             </div>
             <div>
                 <ul>
-                    <li>A Item</li>
+                    {items.map((todoItem) =>  <li>{todoItem}</li> )}
                 </ul>
             </div>
         </div>
